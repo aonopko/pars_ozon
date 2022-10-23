f391e3b9e3f6dfee98f514028fa8479f92e185df
andrii <aonopko.rpu@gmail.com>
2022-10-23 20:57:52 +0300

    work with selenium-stealth

--- pars_file.py
+++ pars_file.py
@@ -1,31 +1,27 @@
-import requests
+from selenium import webdriver
+from selenium_stealth import stealth
 import time
-import undetected_chromedriver
-from bs4 import BeautifulSoup
 
 
-try:
-    driver = undetected_chromedriver.Chrome()
-    driver.get("")
-    time.sleep(15)
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
 
+# options.add_argument("--headless")
 
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=options)
 
-# url = ""
-#
-# headers ={
-#     "Accept": "*/*",
-#     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0"
-# }
-#
-# req = requests.get(url, headers=headers)
-# src = req.text
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Ubuntu",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
 
-
-# with open("index.html", 'w') as file:
-#     file.write(src)
+url = "https://dzen.ru/"
+driver.get(url)
+time.sleep(5)
+driver.quit()
